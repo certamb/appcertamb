904f66d49c775a7ac8d5a4df5909778d4021dad5
carlos <carlosthe19916@gmail.com>
2015-11-17 17:17:21 -0500

    first commit

---
+++ app/scripts/config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Init the application configuration module for AngularJS application
+var ApplicationConfiguration = (function () {
+
+  // Init module configuration options
+  var applicationModuleName = 'mean';
+
+  var applicationModuleVendorDependencies = [
+    /*'ngAnimate',
+    'ui.router',
+    'ui.bootstrap',
+    'ui.utils',
+    //own
+    'ngMessages',
+    'ngSanitize',
+    'restangular',
+    'ui.grid',
+    'ui.grid.edit',
+    'ui.grid.pagination',
+    'ui.grid.selection',
+    'ui.select',
+    'ui.utils.masks',
+    'toastr',
+    'patternfly',
+    'xeditable',
+    'ngFileUpload',
+    'ncy-angular-breadcrumb',
+    'angularSpinner',
+    'angular-ui-view-spinner'*/
+  ];
+
+  // Add a new vertical module
+  var registerModule = function (moduleName, dependencies) {
+    // Create angular module
+    angular.module(moduleName, dependencies || []);
+
+    // Add the module to the AngularJS configuration file
+    angular.module(applicationModuleName).requires.push(moduleName);
+  };
+
+  return {
+    applicationModuleName: applicationModuleName,
+    applicationModuleVendorDependencies: applicationModuleVendorDependencies,
+    registerModule: registerModule
+  };
+
+})();
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the meanApp
+ */
+/*angular.module('meanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });*/
