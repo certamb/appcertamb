8ec7bd0f79d747443f721f697508c2f9cd7f0fc9
carlos <carlosthe19916@gmail.com>
2015-11-24 07:19:28 -0500

    buscar todos los trabajadores

--- app/modules/certamb/controllers/organizacion/trabajador/buscar.client.controller.js
+++ app/modules/certamb/controllers/organizacion/trabajador/buscar.client.controller.js
@@ -2,7 +2,7 @@
 
 /* jshint -W098 */
 angular.module('certamb').controller('Certamb.Organizacion.Trabajador.BuscarController',
-  function ($scope, $state, DIRECCION_REGIONAL, SGDireccionRegional, SGPersonaNatural) {
+  function ($scope, $state, DIRECCION_REGIONAL, SGDireccionRegional, SGTrabajador, SGPersonaNatural) {
 
     $scope.combo = {
       direccionRegional: undefined
@@ -15,6 +15,7 @@ angular.module('certamb').controller('Certamb.Organizacion.Trabajador.BuscarCont
       if ($scope.access.administrarTrabajadores) {
         SGDireccionRegional.$getAll().then(function (response) {
           $scope.combo.direccionRegional = response;
+          $scope.combo.direccionRegional.unshift({denominacion: 'Todos', valor: 'TODO'});
         });
       } else if ($scope.access.administrarTrabajadoresDireccionRegional) {
         $scope.combo.direccionRegional = [DIRECCION_REGIONAL];
@@ -85,28 +86,51 @@ angular.module('certamb').controller('Certamb.Organizacion.Trabajador.BuscarCont
         return;
       }
 
-      var criteria = {
-        filterText: $scope.filterOptions.filterText,
-        filters: [],
-        orders: [],
-        paging: paginationOptions
-      };
-
-      SGDireccionRegional.$new($scope.combo.selected.direccionRegional.id).SGTrabajador().$search(criteria).then(function (response1) {
-        $scope.gridOptions.data = response1.items;
-        $scope.gridOptions.totalItems = response1.totalSize;
-        angular.forEach($scope.gridOptions.data, function (row) {
-          var criteria = {
-            filters: [
-              {name: 'tipoDocumento.abreviatura', value: row.tipoDocumento, operator: 'eq'},
-              {name: 'numeroDocumento', value: row.numeroDocumento, operator: 'eq'}
-            ], paging: {page: 1, pageSize: 10}
-          };
-          SGPersonaNatural.$search(criteria).then(function (response2) {
-            row.persona = response2.items[0];
+      if($scope.combo.selected.direccionRegional.valor === 'TODO'){
+        var criteria = {
+          filterText: $scope.filterOptions.filterText,
+          filters: [],
+          orders: [],
+          paging: paginationOptions
+        };
+        SGTrabajador.$search(criteria).then(function (response1) {
+          $scope.gridOptions.data = response1.items;
+          $scope.gridOptions.totalItems = response1.totalSize;
+          angular.forEach($scope.gridOptions.data, function (row) {
+            var criteria = {
+              filters: [
+                {name: 'tipoDocumento.abreviatura', value: row.tipoDocumento, operator: 'eq'},
+                {name: 'numeroDocumento', value: row.numeroDocumento, operator: 'eq'}
+              ], paging: {page: 1, pageSize: 10}
+            };
+            SGPersonaNatural.$search(criteria).then(function (response2) {
+              row.persona = response2.items[0];
+            });
           });
         });
-      });
+      } else {
+        var criteria = {
+          filterText: $scope.filterOptions.filterText,
+          filters: [],
+          orders: [],
+          paging: paginationOptions
+        };
+        SGDireccionRegional.$new($scope.combo.selected.direccionRegional.id).SGTrabajador().$search(criteria).then(function (response1) {
+          $scope.gridOptions.data = response1.items;
+          $scope.gridOptions.totalItems = response1.totalSize;
+          angular.forEach($scope.gridOptions.data, function (row) {
+            var criteria = {
+              filters: [
+                {name: 'tipoDocumento.abreviatura', value: row.tipoDocumento, operator: 'eq'},
+                {name: 'numeroDocumento', value: row.numeroDocumento, operator: 'eq'}
+              ], paging: {page: 1, pageSize: 10}
+            };
+            SGPersonaNatural.$search(criteria).then(function (response2) {
+              row.persona = response2.items[0];
+            });
+          });
+        });
+      }
 
     };
 
